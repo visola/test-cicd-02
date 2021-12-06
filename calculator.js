d6ad58bcdd846dcfe8528b7cb4731ed6a7c398bd
Vinicius Isola <viniciusisola@gmail.com>
2021-12-06 17:48:57 -0500

    Fix build

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 function calculate(operand1, operand2, operator) {
     switch (operator) {
         case 'sum':
-            return parseFloat(operand1) + parseFloat(operand2) + 2;
+            return parseFloat(operand1) + parseFloat(operand2);
         case 'subtract':
             return parseFloat(operand1) - parseFloat(operand2);
         case 'multiply':
